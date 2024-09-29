9b1a094926656c7ebebdcb702d0bbf8879e779d3
Keith Prakash <71426291+KeithPrakash@users.noreply.github.com>
2024-09-29 17:22:31 +0530

    scaleX 2

--- Parallax Web Project/js/ScaleX.js
+++ Parallax Web Project/js/ScaleX.js
@@ -1,23 +1,63 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-    gsap.registerPlugin(ScrollTrigger,Observer,ScrollToPlugin)
-   });
+
    const tl = gsap.timeline({
     scrollTrigger: {
-      trigger: ".section",
-      start: "center center",  // Start the animation when the section's top hits the center of the viewport
+      trigger: ".mid-image",
+      start: "top 90%",  // Start the animation when the section's top hits the center of the viewport
       end: "bottom center", // End when the section's bottom hits the center of the viewport
       scrub: true, // Smooth animation on scroll
-      markers: true // Enable markers for debugging
+       // Enable markers for debugging
     }
   });
   // First phase: Expand the card from one side
+  tl.to(".mid-image",{
+    rotate:-20,
+    scale:3,
+    duration: 0.1,
+  })
   tl.to(".card", {
-    scaleX: 10, // Expand width on X-axis
-    duration: 1.
+
+    scale: 0.7, // Expand width on X-axis
+    duration: 0.1,
+  
+    rotate:24
+
   });
-  // Second phase: Shrink the card back to its original size
+  tl.to(".mid-image",{
+    rotate:0,
+    scale:1,
+    duration: 0.5,
+  })
+
   tl.to(".card", {
-    scaleX: 1, // Return to original scale
-    duration: 1,
+
+    scale: 2, // Expand width on X-axis
+    duration: 0.2,
+    y:0,
+    rotate:10
+
     
   });
+  tl.to(".mid-image",{
+    rotate:10,
+    scale:1,
+    duration: 0.2,
+  })
+
+  tl.to(".card", {
+
+    scale: 2, // Expand width on X-axis
+    duration: 0.2,
+    y:0,
+    rotate:0
+
+    
+  });
+  tl.to(".mid-image",{
+    rotate:0,
+    scale:1,
+    duration: 0.2,
+  })
+ 
+  // Second phase: Shrink the card back to its original size
+
+ 
